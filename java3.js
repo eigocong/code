d4791d5cb07ce09590353cc21718fff6bff0b647
eigocong <congh618@newschool.edu>
2024-09-08 22:07:56 -0400

    finsihed

--- java3.js
+++ java3.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('.shape-option').forEach(option => {
     option.addEventListener('click', function() {
-        const correctAnswer = document.getElementById("square");
+        const correctAnswer = document.getElementById("heptagon"); 
         document.body.style.backgroundColor = (this === correctAnswer) ? "#05FF00" : "#FF2424";
         document.getElementById("nextButton").style.display = (this === correctAnswer) ? "block" : "none";
     });
